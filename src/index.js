f3504b517a3f1b93b65e71335d8004df2ceec30c
Jared Short <jshort@trek10.com>
2018-03-08 23:58:22 -0500

    firehose, archiving, and transforms

--- src/index.js
+++ src/index.js
@@ -4,21 +4,338 @@ const _ = require('lodash');
 
 class KinesisStream {
     constructor(stream, defaults) {
+        this.stream = stream;
         this.defaults = defaults;
+        this.config = _.merge(
+            {
+                retention: 24,
+                shardCount: 1,
+                keyId: 'alias/aws/kinesis',
+                archiveTransformNewlines: false,
+            },
+            this.defaults,
+            this.stream
+        );
     }
 
     resources() {
-        return [{test: 'thing'}];
-        // kinesis stream
-        // optional firehose
-        // s3 bucket(?) perhaps move this out to its own thing
+        let resources = {};
+        const streamResource = {
+            Type: 'AWS::Kinesis::Stream',
+            Properties: {
+                Name: this.config.name,
+                RetentionPeriodHours: this.config.retention,
+                ShardCount: this.config.shardCount,
+            },
+        };
+
+        if (this.config.keyId) {
+            streamResource.Properties.StreamEncryption = {
+                EncryptionType: 'KMS',
+                KeyId: this.config.keyId,
+            };
+        }
+
+        if (this.config.tags) {
+            streamResource.Properties.Tags = this.config.tags;
+        }
+
+        // If we have archive, we need a firehose and all those managed magics
+        if (this.config.archive) {
+            resources = this._addFirehose(resources, {
+                transformNewlines: this.config.archiveTransformNewlines,
+            });
+        }
+
+        resources[`${this.config.name}KinesisStream`] = streamResource;
+        return resources;
+    }
+
+    _addFirehose(resources, transforms) {
+        const archiveBucketResource = {
+            Type: 'AWS::S3::Bucket',
+            Properties: {
+                BucketEncryption: {
+                    ServerSideEncryptionConfiguration: [
+                        {
+                            ServerSideEncryptionByDefault: {
+                                SSEAlgorithm: 'aws:kms',
+                            },
+                        },
+                    ],
+                },
+            },
+        };
+
+        if (this.config.archiveBucket) {
+            archiveBucketResource.Properties.BucketName = this.config.archiveBucket;
+        }
+        resources['KinesisStreamManagerArchiveBucket'] = archiveBucketResource;
+
+        const firehoseIAMRole = {
+            Type: 'AWS::IAM::Role',
+            Properties: {
+                AssumeRolePolicyDocument: {
+                    Version: '2012-10-17',
+                    Statement: [
+                        {
+                            Effect: 'Allow',
+                            Principal: {
+                                Service: 'firehose.amazonaws.com',
+                            },
+                            Action: 'sts:AssumeRole',
+                            Condition: {
+                                StringEquals: {
+                                    'sts:ExternalId': {Ref: 'AWS::AccountId'},
+                                },
+                            },
+                        },
+                    ],
+                },
+                Policies: [
+                    {
+                        PolicyName: 'firehose-archive-kinesis',
+                        PolicyDocument: {
+                            Version: '2012-10-17',
+                            Statement: [
+                                {
+                                    Effect: 'Allow',
+                                    Action: [
+                                        's3:AbortMultipartUpload',
+                                        's3:GetBucketLocation',
+                                        's3:GetObject',
+                                        's3:ListBucket',
+                                        's3:ListBucketMultipartUploads',
+                                        's3:PutObject',
+                                    ],
+                                    Resource: [
+                                        {
+                                            'Fn::Sub':
+                                                '${KinesisStreamManagerArchiveBucket.Arn}',
+                                        },
+                                        {
+                                            'Fn::Sub':
+                                                '${KinesisStreamManagerArchiveBucket.Arn}/*',
+                                        },
+                                    ],
+                                },
+                                {
+                                    Effect: 'Allow',
+                                    Action: [
+                                        'kinesis:DescribeStream',
+                                        'kinesis:GetShardIterator',
+                                        'kinesis:GetRecords',
+                                    ],
+                                    Resource: {
+                                        'Fn::Sub':
+                                            'arn:aws:kinesis:${AWS::Region}:${AWS::AccountId}:stream/*',
+                                    },
+                                },
+                                {
+                                    Effect: 'Allow',
+                                    Action: [
+                                        'kms:Decrypt',
+                                        'kms:GenerateDataKey',
+                                    ],
+                                    Resource: [
+                                        {
+                                            'Fn::Sub': `arn:aws:kms:region:\${AWS::Region}:key/*`, // TODO: security review
+                                        },
+                                    ],
+                                    Condition: {
+                                        StringEquals: {
+                                            'kms:ViaService': {
+                                                'Fn::Sub':
+                                                    's3.${AWS::Region}.amazonaws.com',
+                                            },
+                                        },
+                                        StringLike: {
+                                            'kms:EncryptionContext:aws:s3:arn': {
+                                                'Fn::Sub':
+                                                    '${KinesisStreamManagerArchiveBucket}/*',
+                                            },
+                                        },
+                                    },
+                                },
+                                {
+                                    Effect: 'Allow',
+                                    Action: ['logs:PutLogEvents'],
+                                    Resource: ['*'],
+                                },
+                            ],
+                        },
+                    },
+                ],
+            },
+        };
+        resources['FirehoseIAMRole'] = firehoseIAMRole;
+
+        const firehoseCloudWatchLogs = {
+            Type: 'AWS::Logs::LogGroup',
+            Properties: {
+                RetentionInDays: 30,
+            },
+        };
+        resources['FirehoseCloudWatchLogs'] = firehoseCloudWatchLogs;
+
+        const firehoseResource = {
+            Type: 'AWS::KinesisFirehose::DeliveryStream',
+            Properties: {
+                DeliveryStreamName: `${this.config.name}KinesisFirehose`,
+                DeliveryStreamType: 'KinesisStreamAsSource',
+                KinesisStreamSourceConfiguration: {
+                    KinesisStreamARN: {
+                        'Fn::Sub': `\${${this.config.name}KinesisStream.Arn}`,
+                    },
+                    RoleARN: {'Fn::Sub': '${FirehoseIAMRole.Arn}'},
+                },
+                ExtendedS3DestinationConfiguration: {
+                    BucketARN: {
+                        'Fn::Sub': '${KinesisStreamManagerArchiveBucket.Arn}',
+                    },
+                    BufferingHints: {
+                        IntervalInSeconds: 60,
+                        SizeInMBs: 1,
+                    },
+                    CloudWatchLoggingOptions: {
+                        Enabled: true,
+                        LogGroupName: {Ref: 'FirehoseCloudWatchLogs'},
+                        LogStreamName: this.config.name,
+                    },
+                    CompressionFormat: 'GZIP',
+                    Prefix: `${this.config.name}Stream/`,
+                    RoleARN: {'Fn::Sub': '${FirehoseIAMRole.Arn}'},
+                },
+            },
+        };
+
+        if (transforms.transformNewlines) {
+            const processorLambdaIAMRole = {
+                Type: 'AWS::IAM::Role',
+                Properties: {
+                    AssumeRolePolicyDocument: {
+                        Version: '2012-10-17',
+                        Statement: [
+                            {
+                                Effect: 'Allow',
+                                Principal: {
+                                    Service: ['lambda.amazonaws.com'],
+                                },
+                                Action: ['sts:AssumeRole'],
+                            },
+                        ],
+                    },
+                    Policies: [
+                        {
+                            PolicyName: 'create-log-streams',
+                            PolicyDocument: {
+                                Version: '2012-10-17',
+                                Statement: [
+                                    {
+                                        Effect: 'Allow',
+                                        Action: [
+                                            'logs:CreateLogGroup',
+                                            'logs:CreateLogStream',
+                                            'logs:PutLogEvents',
+                                            'logs:DescribeLogStreams',
+                                        ],
+                                        Resource: ['*'],
+                                    },
+                                ],
+                            },
+                        },
+                    ],
+                    Path: '/',
+                },
+            };
+            resources['ProcessLambdaIAMRole'] = processorLambdaIAMRole;
+            const lambdaArchiveTransformNewlines = {
+                Type: 'AWS::Lambda::Function',
+                Properties: {
+                    Code: {
+                        ZipFile: {
+                            'Fn::Join': [
+                                '\n',
+                                [
+                                    'import base64',
+                                    'def handler(event, context):',
+                                    '    output = []',
+                                    "    for record in event['records']:",
+                                    "       payload = base64.b64decode(record['data'])+str.encode('\\n')",
+                                    '       output_record = {',
+                                    "           'recordId': record['recordId'],",
+                                    "           'result': 'Ok',",
+                                    "           'data': base64.b64encode(payload).decode('utf-8')",
+                                    '       }',
+                                    '       output.append(output_record)',
+                                    "    return {'records': output}",
+                                ],
+                            ],
+                        },
+                    },
+                    Handler: 'index.handler',
+                    Runtime: 'python3.6',
+                    Role: {'Fn::Sub': '${ProcessLambdaIAMRole.Arn}'},
+                    Timeout: 60,
+                },
+            };
+
+            firehoseResource.Properties.ExtendedS3DestinationConfiguration.ProcessingConfiguration = {
+                Enabled: true,
+                Processors: [
+                    {
+                        Type: 'Lambda',
+                        Parameters: [
+                            {
+                                ParameterName: 'LambdaArn',
+                                ParameterValue: {
+                                    'Fn::Sub': '${ArchiveNewlineTransform.Arn}',
+                                },
+                            },
+                            {
+                                ParameterName: 'NumberOfRetries',
+                                ParameterValue: 3,
+                            },
+                            {
+                                ParameterName: 'RoleArn',
+                                ParameterValue: {
+                                    'Fn::Sub': '${FirehoseIAMRole.Arn}',
+                                },
+                            },
+                        ],
+                    },
+                ],
+            };
+
+            resources[
+                'ArchiveNewlineTransform'
+            ] = lambdaArchiveTransformNewlines;
+            const lambdaFirehosePermission = {
+                Effect: 'Allow',
+                Action: [
+                    'lambda:InvokeFunction',
+                    'lambda:GetFunctionConfiguration',
+                ],
+                Resource: [
+                    {
+                        'Fn::Sub': '${ArchiveNewlineTransform.Arn}*',
+                    },
+                ],
+            };
+            firehoseIAMRole.Properties.Policies[0].PolicyDocument.Statement.push(
+                lambdaFirehosePermission
+            );
+        }
+
+        resources[`${this.config.name}KinesisFirehose`] = firehoseResource;
+        return resources;
     }
 }
 
 /****
  * Example Configs
  * custom:
- *   kinesis-streams:
+ *   kinesisStreams:
  *     defaults: (optional global overrides)
  *       archiveBucket: {RandomlyNamedByCFN}
  *       archive: false
@@ -26,14 +343,16 @@ class KinesisStream {
  *       encryptionKey: alias/aws/kms
  *       retention: 24
  *       shardCount: 1
+ *       archiveTransformNewlines: false
  *     streams:
  *       - name: MyStream (required)
  *         archiveBucket: (optional)
  *         archive: true (optional)
- *         encryption: false (optional)
- *         encryptionKey: alias/aws/kinesis (optional)
+ *         keyId: alias/aws/kinesis (optional)
  *         retention: 24 (optional)
  *         shardCount: 1 (optional)
+ *         archiveTransformNewlines: false
+ *         tags:
  ***/
 
 class KinesisStreamManager {
@@ -49,31 +368,26 @@ class KinesisStreamManager {
     }
 
     beforeCompileFunctions() {
-        if (!_.has(this.serverless.service, 'custom.kinesis-streams.streams')) {
+        if (!_.has(this.serverless.service, 'custom.kinesisStreams.streams')) {
             return;
         }
 
         let defaults = {};
-        if (_.has(this.serverless.service, 'custom.kinesis-streams.defaults')) {
-            defaults = this.serverless.service.custom['kinesis-streams']
-                .defaults;
+        if (_.has(this.serverless.service, 'custom.kinesisStreams.defaults')) {
+            defaults = this.serverless.service.custom.kinesisStreams.defaults;
         }
 
-        const streams = this.serverless.service.custom[
-            'kinesis-streams'
-        ].streams.map(stream => new KinesisStream(stream, defaults));
-
-        streams.forEach(stream =>
-            stream.resources().forEach(resource => {
-                // Test with CloudFormation overrides
-                _.merge(
-                    this.serverless.service.provider
-                        .compiledCloudFormationTemplate.Resources,
-                    resource
-                );
-            })
+        const streams = this.serverless.service.custom.kinesisStreams.streams.map(
+            stream => new KinesisStream(stream, defaults)
         );
-        this.cli.log(JSON.stringify(streams));
+
+        streams.forEach(stream => {
+            _.merge(
+                this.serverless.service.provider.compiledCloudFormationTemplate
+                    .Resources,
+                stream.resources()
+            );
+        });
     }
 }
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,35 +2,161 @@
 
 const KinesisStreamManager = require('../');
 
-// it('creates CloudFormation configuration', () => {
-//     let config = {
-//         getProvider: () => ({ getRegion: () => 'test-region' }),
-//         service: {
-//             custom: {
-//                 'sqs-alarms': [
-//                     {
-//                         queue: 'test-queue',
-//                         topic: 'test-topic',
-//                         thresholds: [1, 2, 3]
-//                     }
-//                 ]
-//             },
-//             provider: {
-//                 compiledCloudFormationTemplate: {
-//                     Resources: {}
-//                 }
-//             }
-//         }
-//     }
+it('creates basic CloudFormation with minimal config', () => {
+    let config = {
+        service: {
+            custom: {
+                kinesisStreams: {
+                    streams: [
+                        {name: 'Test'}
+                    ]
+                },
+            },
+            provider: {
+                compiledCloudFormationTemplate: {
+                    Resources: {},
+                },
+            },
+        },
+    };
 
-//     const test = new Plugin(config)
-//     test.beforeCompileFunctions()
+    const test = new KinesisStreamManager(config);
+    test.beforeCompileFunctions();
 
-//     const data = config.service.provider.compiledCloudFormationTemplate.Resources
+    const data =
+        config.service.provider.compiledCloudFormationTemplate.Resources;
 
-//     expect(data).toHaveProperty('testqueueMessageAlarm3')
-// })
+    expect(data).toHaveProperty('TestKinesisStream');
+    expect(data.TestKinesisStream).toEqual({
+        Type: 'AWS::Kinesis::Stream',
+        Properties: {
+            Name: 'Test',
+            RetentionPeriodHours: 24,
+            ShardCount: 1,
+            StreamEncryption: {
+                EncryptionType: 'KMS',
+                KeyId: 'alias/aws/kinesis',
+            },
+        },
+    });
+});
 
+
+it('supports multiple streams', () => {
+    let config = {
+        service: {
+            custom: {
+                kinesisStreams: {
+                    streams: [
+                        {name: 'Foo'},
+                        {name: 'Bar'}
+                    ]
+                },
+            },
+            provider: {
+                compiledCloudFormationTemplate: {
+                    Resources: {},
+                },
+            },
+        },
+    };
+
+    const test = new KinesisStreamManager(config);
+    test.beforeCompileFunctions();
+
+    const data =
+        config.service.provider.compiledCloudFormationTemplate.Resources;
+
+    expect(data).toHaveProperty('FooKinesisStream');
+    expect(data.FooKinesisStream).toEqual({
+        Type: 'AWS::Kinesis::Stream',
+        Properties: {
+            Name: 'Foo',
+            RetentionPeriodHours: 24,
+            ShardCount: 1,
+            StreamEncryption: {
+                EncryptionType: 'KMS',
+                KeyId: 'alias/aws/kinesis',
+            },
+        },
+    });
+    expect(data).toHaveProperty('BarKinesisStream');
+    expect(data.BarKinesisStream).toEqual({
+        Type: 'AWS::Kinesis::Stream',
+        Properties: {
+            Name: 'Bar',
+            RetentionPeriodHours: 24,
+            ShardCount: 1,
+            StreamEncryption: {
+                EncryptionType: 'KMS',
+                KeyId: 'alias/aws/kinesis',
+            },
+        },
+    });
+});
+
+it('allows global defaults and stream based overrides', () => {
+    let config = {
+        service: {
+            custom: {
+                kinesisStreams: {
+                    defaults: {
+                        retention: 72,
+                        shardCount: 4
+                    },
+                    streams: [
+                        {
+                            name: 'Foo',
+                            retention: 12
+                        },
+                        {
+                            name: 'Bar',
+                            shardCount: 8
+                        }
+                    ]
+                },
+            },
+            provider: {
+                compiledCloudFormationTemplate: {
+                    Resources: {},
+                },
+            },
+        },
+    };
+
+    const test = new KinesisStreamManager(config);
+    test.beforeCompileFunctions();
+
+    const data =
+        config.service.provider.compiledCloudFormationTemplate.Resources;
+
+    expect(data).toHaveProperty('FooKinesisStream');
+    expect(data.FooKinesisStream).toEqual({
+        Type: 'AWS::Kinesis::Stream',
+        Properties: {
+            Name: 'Foo',
+            RetentionPeriodHours: 12,
+            ShardCount: 4,
+            StreamEncryption: {
+                EncryptionType: 'KMS',
+                KeyId: 'alias/aws/kinesis',
+            },
+        },
+    });
+    expect(data).toHaveProperty('BarKinesisStream');
+    expect(data.BarKinesisStream).toEqual({
+        Type: 'AWS::Kinesis::Stream',
+        Properties: {
+            Name: 'Bar',
+            RetentionPeriodHours: 72,
+            ShardCount: 8,
+            StreamEncryption: {
+                EncryptionType: 'KMS',
+                KeyId: 'alias/aws/kinesis',
+            },
+        },
+    });
+});
 it('does not fail without configuration', () => {
     let config = {
         getProvider: () => ({getRegion: () => 'test-region'}),
@@ -50,5 +176,5 @@ it('does not fail without configuration', () => {
     const data =
         config.service.provider.compiledCloudFormationTemplate.Resources;
 
-    expect(data).not.toHaveProperty('ShouldntExistStream');
+    expect(data).toEqual({})
 });
